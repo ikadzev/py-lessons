490ff1a550bbed77c9c2e52105bec7d71adffc91
Valentin <ikadzev@mail.ru>
2023-10-21 23:07:22 +0700

    Updated mini4.py

--- minies/mini4.py
+++ minies/mini4.py
@@ -1,4 +1,5 @@
-dict = {"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832, "Kuznebcov": 97832}
+dict = {("Ivanov", 1): 97832, "Petrov": 55521, "Kuznecov": 97832, "Kuznebcov": 97832}
+
 tcid = {}
 for value, key in dict.items():
     if key in tcid:
@@ -9,5 +10,5 @@ for value, key in dict.items():
         else:
             tcid[key] = (tcid[key], value)
     else:
-        tcid[key] = value
+        tcid[key] = value if type(value) != tuple else (value,)
 print(tcid)
