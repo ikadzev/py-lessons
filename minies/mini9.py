7f392a147f472066eb68d72220c62249d7db75e1
Valentin <ikadzev@mail.ru>
2023-10-30 00:04:50 +0700

    Updated mini9.py

--- minies/mini9.py
+++ minies/mini9.py
@@ -1,22 +1,21 @@
 def format_table(list1, list2, list3):
-    length = 11
-    for str in list2:
-        length += len(str) + 3
-    maxx = max([len(i) for i in list1])
-
-    print(f'| {"Benchmark":<{max(9, maxx)}} |', end = '')
+    maxes = [max(9, max([len(i) for i in list1]))]
+    for n in range(len(list2)):
+        maxes.append(max(len(list2[n]), max([len(str(list3[j][n])) for j in range(len(list3))])))
+    print(f'| {"Benchmark":<{maxes[0]}} | ', end = '')
     for i in range(len(list2)):
-        print(f' {list2[i]} |', end='')
+        print(f' {list2[i]:<{maxes[i+1]}} |', end='')
     
-    print('\n|' + '-' * (length + max(0, maxx-9)) + '|')
+    print('\n|' + '-' * (sum(maxes) + 3 * len(list2) + 3) + '|')
+
     for i in range(len(list1)):
-        print(f'| {list1[i]:<{max(9, maxx)}} | ', end='')
+        print(f'| {list1[i]:<{maxes[0]}} | ', end='')
         for j in range(len(list3[i])):
-            print(f'{list3[i][j]:<{len(list2[j])}} | ', end='')
+            print(f' {list3[i][j]:<{maxes[j+1]}} |', end='')
         print()
     
 
 
 format_table(["best case", "worst case"],
              ["quick sort", "merge sort", "bubble sort"],
-             [[1.23, 1.5345345345346, 2.00], [3.3, 2.9, 3.9]])
+             [[1.23, 1.5345345345346, 2.00], [3.3, 2.9, 3.9]]) 
