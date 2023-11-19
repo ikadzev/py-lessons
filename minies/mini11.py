a8115bf93232d55c6a02f558236c92e5337da239
Valentin <ikadzev@mail.ru>
2023-11-19 22:59:30 +0700

    Changed mini11.py

--- minies/mini11.py
+++ minies/mini11.py
@@ -4,48 +4,33 @@ class Counter:
         self.step = step
     def increment(self):
         self.count += self.step
-        
-# past_cls = None
-# 
-# def singleton(cls):
-#     global past_cls
-#     if not past_cls:
-#         past_cls = cls
-#     print(past_cls)
-#     return past_cls
-# 
-# @singleton
-# class GlobalCounter(Counter):
-#     pass
-
-# ВЫШЕ попытка сделать через декоратор
-
 
 class Singleton:
     singleton = None
-    # def __init__(self, *args, **kwargs):
-    #     # print('self' if self else 'error with self!', 'and singleton eq to self' if Singleton.singleton == self else 'and singelton not eq to self' if Singleton.singleton else 'and singleton is none')
-    #     if not Singleton.singleton:
-    #         # print('Standart init')
-    #         super().__init__(*args, **kwargs)
-    #         Singleton.singleton = self
-    #     else:
-    #         # print('Singelton init')
-    #         self = Singleton.singleton
-    #     # print('self' if self else 'error with self!', 'and singleton eq to self' if Singleton.singleton == self else 'and singelton not eq to self' if Singleton.singleton else 'and singleton is none')
-    def __new__(cls):
+    s_check = False
+    singleton_args = None
+    def __new__(cls, *args, **kwargs):
         if not cls.singleton:
             cls.singleton = super().__new__(cls)
+        else:
+            Singleton.s_check = True
+            Singleton.singleton_args = list(vars(cls.singleton).values())
         return cls.singleton
+    def __init__(self, *args, **kwargs):
+        if Singleton.s_check:
+            super().__init__(*Singleton.singleton_args)
+        else:
+            super().__init__(*args, **kwargs)
 
 class GlobalCounter(Singleton, Counter):
     pass
 
-gc1 = GlobalCounter()
-gc2 = GlobalCounter()
-
+gc1 = GlobalCounter(1, 2)
 print(vars(gc1))
 gc1.increment()
+print(vars(gc1))
+
+gc2 = GlobalCounter()
 print(vars(gc2))
 gc2.increment()
 print(vars(gc1))
